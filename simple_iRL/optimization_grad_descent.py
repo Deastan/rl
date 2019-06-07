8cea7085ac5890ef1d801a84a9f820a6f6b7d815
Jonathan <jonathanburkhard@gmail.com>
2019-06-07 16:03:06 +0200

    add some stuff

--- simple_iRL/optimization_grad_descent.py
+++ simple_iRL/optimization_grad_descent.py
@@ -1,3 +1,75 @@
+# Jonathan Burkhard
+# IRL
+# help link :
+#               - https://towardsdatascience.com/gradient-descent-in-python-a0d07285742f
+#               - https://www.freecodecamp.org/news/understanding-gradient-descent-the-most-popular-ml-algorithm-a66c0d97307f/
+#               - https://ml-cheatsheet.readthedocs.io/en/latest/gradient_descent.html
+
+
+'''
+From https://docs.python.org/3/library/math.html
+
+- math.exp(x):
+    Return e raised to the power x, where e = 2.718281… is the base 
+    of natural logarithms. This is usually more accurate 
+    than math.e ** x or pow(math.e, x).
+
+- math.log(x[, base]):
+    With one argument, return the natural logarithm of x (to base e).
+    With two arguments, return the logarithm of x to the given base, 
+    calculated as log(x)/log(base).
+
+- math.log1p(x):
+    Return the natural logarithm of 1+x (base e). The result is 
+    calculated in a way which is accurate for x near zero.
+
+- math.pow(x, y):
+    Return x raised to the power y. Exceptional cases follow Annex 
+    ‘F’ of the C99 standard as far as possible. In particular, 
+    pow(1.0, x) and pow(x, 0.0) always return 1.0, even when x is a 
+    zero or a NaN. If both x and y are finite, x is negative, and y 
+    is not an integer then pow(x, y) is undefined, and raises 
+    ValueError.
+
+    Unlike the built-in ** operator, math.pow() converts both its 
+    arguments to type float. Use ** or the built-in pow() function 
+    for computing exact integer powers.
+
+-math.sqrt(x):
+    Return the square root of x.
+
+math.acos(x):
+    Return the arc cosine of x, in radians.
+
+math.asin(x):
+    Return the arc sine of x, in radians.
+
+math.atan(x):
+    Return the arc tangent of x, in radians.
+
+math.atan2(y, x):
+    Return atan(y / x), in radians. The result is between -pi and pi. 
+    The vector in the plane from the origin to point (x, y) makes this 
+    angle with the positive X axis. The point of atan2() is that the 
+    signs of both inputs are known to it, so it can compute the correct 
+    quadrant for the angle. For example, atan(1) and atan2(1, 1) are 
+    both pi/4, but atan2(-1, -1) is -3*pi/4.
+
+math.cos(x):
+    Return the cosine of x radians.
+
+math.hypot(x, y):
+    Return the Euclidean norm, sqrt(x*x + y*y). This is the length of 
+    the vector from the origin to point (x, y).
+
+math.sin(x):
+    Return the sine of x radians.
+
+math.tan(x):
+    Return the tangent of x radians.
+
+
+'''
 import random
 import time
 import gym
@@ -35,7 +107,7 @@ def load(name="demos_default_name"):
         return empty
 
 # Return the calculated cost function
-def cal_cost_function():
+def cal_cost_function(x, y, theta):
     '''
     Calculates the cost function for the given parameters.
     
@@ -56,12 +128,17 @@ def grad_descent(learning_rate=0.01, iterations=100):
     Calculates iteratively the gradient
 
     Pseudo-code:
-    The gradient tells us the incline or slope of the cost function. Hence, to minimize the cost function, we move in the direction opposite to the gradient.
+    The gradient tells us the incline or slope of the cost function. Hence, to 
+    minimize the cost function, we move in the direction opposite to the gradient.
 
     1) Initialize the weights W randomly.
-    2) Calculate the gradients G of cost function w.r.t parameters. This is done using partial differentiation: G = ∂J(W)/∂W. The value of the gradient G depends on the inputs, the current values of the model parameters, and the cost function. You might need to revisit the topic of differentiation if you are calculating the gradient by hand.
+    2) Calculate the gradients G of cost function w.r.t parameters. This is done using partial 
+        differentiation: G = ∂J(W)/∂W. The value of the gradient G depends on the inputs, the 
+        current values of the model parameters, and the cost function. You might need to revisit 
+        the topic of differentiation if you are calculating the gradient by hand.
     3) Update the weights by an amount proportional to G, i.e. W = W - ηG
-    4) Repeat until the cost J(w) stops reducing, or some other pre-defined termination criteria is met.
+    4) Repeat until the cost J(w) stops reducing, or some other pre-defined 
+        termination criteria is met.
 
     Input:
 
@@ -90,7 +167,7 @@ def main():
 
     # cal_cost_function()
     grad_descent()
-    
+
     print("Main end!")
 
 #***********************************
